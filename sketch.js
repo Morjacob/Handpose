e75fb5f7a0ea94956d2f09543f61c7e44d3abca1
Morgan Jacob <jacobmmori@gmail.com>
2024-11-25 21:31:09 -0500

    experimenting

--- sketch.js
+++ sketch.js
@@ -1,47 +1,44 @@
+let handPose;
+let video;
+let hands = [];
+
 function preload() {
-    handPose = ml5.handPose();
-  }
+  // Load the handPose model
+  handPose = ml5.handPose();
+}
 
-  let video;
-
-  function setup() {
-    createCanvas(640, 480);
-    // Create the video and hide it
+function setup() {
+  createCanvas(640, 480);
+  // Create the webcam video and hide it
   video = createCapture(VIDEO);
   video.size(640, 480);
   video.hide();
+  // start detecting hands from the webcam video
+  handPose.detectStart(video, gotHands);
 }
 
-let hands = [];
+function draw() {
+  // Draw the webcam video
+  image(video, 0, 0, width, height);
 
-function setup() {
-    // ...
-    video.hide();
-  
-    // Start detecting hands from the webcam video
-    handPose.detectStart(video, gotHands);
-  }
-  
-  // Callback function for when handPose outputs data
-function gotHands(results) {
-    // Save the output to the hands variable
-    hands = results;
-  }
-  
-  function draw() {
-    image(video, 0, 0, width, height);
-  
-      // Draw all the tracked hand points
+  // Draw all the tracked hand points
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
-
     for (let j = 0; j < hand.keypoints.length; j++) {
-        let keypoint = hand.keypoints[j];
-  
-        fill(0, 255, 0);
-        noStroke();
-        circle(keypoint.x, keypoint.y, 10);
-      }
+      let keypoint = hand.keypoints[j];
+      fill(0, 255, 0);
+      noStroke();
+      circle(keypoint.x, keypoint.y, 10);
     }
   }
+}
+
+// Callback function for when handPose outputs data
+function gotHands(results) {
+  // save the output to the hands variable
+  hands = results;
+}
+
+  
+
   
